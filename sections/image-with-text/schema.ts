c67402df82b53a80d3c429c1309b6c05436ed584
Nabeel Abrahams <88242933+NabeelAbrahams@users.noreply.github.com>
2023-10-04 15:47:55 +0200

    init challenge 2 - create a new section + types

---
+++ sections/image-with-text/schema.ts
@@ -0,0 +1,4 @@
+import { sectionGlobals } from "globals/settings/section-globals";
+import { fontTypeRange } from "globals/settings/type-range";
+import { ImageWithTextSection } from "types/sections";
+import { ShopifySection } from "types/shopify";
